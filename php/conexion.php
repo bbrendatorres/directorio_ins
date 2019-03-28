4bc2d7c4f43cf51d0f9fad337e9574b8f450a07c
BBerenice <bbrenda.torres19@gmail.com>
2019-03-28 11:11:49 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,13 +1,15 @@
-<?php 
-function conectar(){
-	$user="root";
-	$pass="";
-	$server="localhost:80";
-	$db="directorio";
-	$con=mysql_connect($server,$user,$pass) or die ("Error en conexión a la bd".mysql_error());
+<?php
+$enlace = mysqli_connect('127.0.0.1', 'root', '', 'directorio');
 
-	mysql_select_db($db,$con);
-	return  $con;
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
- ?>
 
+echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
+echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+
+mysqli_close($enlace);
+?>
